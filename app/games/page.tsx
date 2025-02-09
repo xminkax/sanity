7b63724a3af49494de6307344b5c56a922b0c2f4
xminkax <xminkax@gmail.com>
2025-02-09 14:39:38 +0100

    Continue with refactoring

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,18 +1,17 @@
 import SnakeGame from "@/src/SnakeGame";
-import NextLevel from "@/src/NextLevel";
-import GameOver from "@/src/GameOver";
 
 export default function Games() {
-    return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                padding: "8rem",
-            }}
-        >
-            {/*<h1>Snake Game</h1>*/}
-            <GameOver/>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        padding: "8rem",
+      }}
+    >
+      {/*<h1>Snake Game</h1>*/}
+      {/*<GameOver/>*/}
+      <SnakeGame/>
+    </div>
+  );
 }
