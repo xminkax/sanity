0e91a7e75c8e547c3d81a5aa986586272db63d1d
xminkax <xminkax@gmail.com>
2025-02-11 17:58:08 +0100

    Finish first version of layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,41 @@
 import React from "react";
 
+const welcomeTexts: string[] = [
+  "Welcome!",
+  "I am monika",
+  "I enjoy coding"
+]
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex justify-center items-center h-screen">
+      <section>
+        <div className="flex justify-center">
+          <div className="typewriter">
+            {welcomeTexts.map((item: string) =>
+              <h1 key={item} className="font-bold text-4xl sm:text-6xl">
+                {item}
+              </h1>
+            )}
+          </div>
+        </div>
+        <div>
+          <p className="mt-6 text-2xl leading-8 text-center">Can you catch the yellow
+            star?
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a href="#"
+               className="z-2 inline-block px-6 py-3 text-white font-semibold text-lg bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 rounded-lg shadow-lg transform transition-all hover:scale-105 hover:from-purple-600 hover:via-purple-700 hover:to-purple-800 hover:shadow-xl">
+              No-coming soon<span
+              aria-hidden="true">→</span></a>
+            <a href="#"
+               className="inline-block px-6 py-3 text-white font-semibold text-lg bg-gradient-to-r from-gray-700 via-gray-800 to-black rounded-lg shadow-lg transform transition-all hover:scale-105 hover:from-gray-800 hover:via-gray-900 hover:to-black hover:shadow-xl">
+              Learn More-coming soon<span
+              aria-hidden="true">→</span></a>
 
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-             aria-hidden="true">
-          <div
-            // bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
-        </div>
-        <div className="mx-auto max-w-2xl py-16">
-          <div className="" style={{display: "flex", justifyContent: "center"}}>
-            <div className="typewriter">
-              <h1 className="text-4xl font-bold text-white-900 sm:text-6xl">
-                Welcome!</h1>
-              <h1 className="text-4xl font-bold text-white-900 sm:text-6xl">
-                I am Monika</h1>
-              <h1 className="text-4xl font-bold text-white-900 sm:text-6xl">
-                I enjoy coding</h1>
-            </div>
-          </div>
-          <div>
-            <div>
-              <p className="mt-6 text-2xl leading-8 text-white-600 text-center">Can you catch the yellow
-                star?
-              </p>
-              <div className="mt-10 flex items-center justify-center gap-x-6">
-                <a href="#"
-                   className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">No
-                  (coming soon)</a>
-                <a href="#" className="text-sm font-semibold leading-6 text-white-900">Learn more (coming soon)<span
-                  aria-hidden="true">→</span></a>
-              </div>
-            </div>
           </div>
         </div>
-      </div>
+      </section>
     </main>
-  );
+  )
+    ;
 }
