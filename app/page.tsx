94709edd4a07ec4d51b8663b0d7943297140c5d8
xminkax <xminkax@gmail.com>
2025-02-12 10:20:32 +0100

    add colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from "react";
 
 const welcomeTexts: string[] = [
@@ -10,7 +11,7 @@ export default function Home() {
     <main className="flex justify-center items-center h-screen">
       <section>
         <div className="flex justify-center">
-          <div className="typewriter">
+          <div className="typewriter flex-row flex">
             {welcomeTexts.map((item: string) =>
               <h1 key={item} className="font-bold text-4xl sm:text-6xl">
                 {item}
@@ -19,19 +20,16 @@ export default function Home() {
           </div>
         </div>
         <div>
-          <p className="mt-6 text-2xl leading-8 text-center">Can you catch the yellow
+          <p className="mt-6 sm:text-3xl text-2xl leading-8 text-center">Can you catch the yellow
             star?
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a href="#"
-               className="z-2 inline-block px-6 py-3 text-white font-semibold text-lg bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 rounded-lg shadow-lg transform transition-all hover:scale-105 hover:from-purple-600 hover:via-purple-700 hover:to-purple-800 hover:shadow-xl">
-              No-coming soon<span
-              aria-hidden="true">→</span></a>
-            <a href="#"
-               className="inline-block px-6 py-3 text-white font-semibold text-lg bg-gradient-to-r from-gray-700 via-gray-800 to-black rounded-lg shadow-lg transform transition-all hover:scale-105 hover:from-gray-800 hover:via-gray-900 hover:to-black hover:shadow-xl">
-              Learn More-coming soon<span
-              aria-hidden="true">→</span></a>
-
+            <a onClick={() => alert("coming soon")} href="#"
+               className="inline-block px-6 py-3 text-white font-semibold text-lg bg-gradient-to-r from-indigo-800 via-purple-700 to-indigo-900 rounded-lg transform transition-all hover:scale-105 hover:from-purple-800 hover:via-indigo-700 hover:to-purple-900">
+              No, what?</a>
+            <a onClick={() => alert("coming soon")} href="#"
+               className="inline-block px-6 py-3 text-white font-semibold text-lg bg-gradient-to-r from-gray-800 via-gray-700 to-gray-900 rounded-lg transform transition-all hover:scale-105 hover:from-gray-700 hover:via-gray-600 hover:to-gray-800">
+              Yes</a>
           </div>
         </div>
       </section>
