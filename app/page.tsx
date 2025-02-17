7e240d3b47ddf2151efedccfddf0945f71dc1a40
xminkax <xminkax@gmail.com>
2025-02-17 14:01:13 +0100

    working version

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
             </h1>
           )}
         </div>
-        <div style={{padding: "4rem 0"}}>
+        <div className="sm:py-10 py-8">
           <p className="mt-6 sm:text-3xl text-2xl leading-8 text-center">Can you see the yellow star?
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
