c4680ae5ba474b441c3484431f6ef51bca9c3589
xminkax <xminkax@gmail.com>
2025-03-21 10:27:34 +0100

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const orbitron = Orbitron({
 });
 
 export const metadata: Metadata = {
-  title: "Welcome to my website",
+  title: "Monika Kindernayova",
   description: "I am a software engineer with over 12 years of professional experience.",
   openGraph: {
     images: [
